431d9f948d8cfd314c74a07d23f14e1062a066e0
Saylox <nathan.fluckiger@hotmail.ch>
2020-01-16 15:23:00 +0100

    Adding the second api

---
+++ services/officialLeague - Connexion/src/main/java/ch/heigvd/amt/api/endpoints/UsersApiController.java
@@ -0,0 +1,54 @@
+package ch.heigvd.amt.api.endpoints;
+
+import com.auth0.jwt.exceptions.JWTCreationException;
+
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-07-26T19:36:34.802Z")
+
+@Controller
+public class UsersApiController implements ConnectionApi {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthenticationService authentication;
+
+    @Autowired
+    AuthorizationService authorization;
+
+    @Override
+    public ResponseEntity<Token> login(@ApiParam(value = "" ,required=true )  @Valid @RequestBody Credentials credentials) {
+        if(userRepository.existsById(credentials.getEmail())) {
+
+            User user = toUser(userRepository.findById(credentials.getEmail()).get());
+            if(authentication.checkPassword(credentials.getPassword(), userRepository.findById(credentials.getEmail()).get().getPassword())) {
+                try {
+                    Token token = authorization.generateToken(user);
+                    return ResponseEntity.ok(token);
+                } catch (JWTCreationException e) {
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+                }
+            }
+        }
+
+        return ResponseEntity.status(401).build();
+    }
+
+    private User toUser(UserEntity entity) {
+        User user = new User();
+        user.setEmail(entity.getEmail());
+        user.setFirstName(entity.getFirstName());
+        user.setLastName(entity.getLastName());
+        user.setPassword(entity.getPassword());
+        return user;
+    }
+}
---
+++ services/officialLeague - Connexion/src/main/java/ch/heigvd/amt/entities/GameEntity.java
@@ -0,0 +1,66 @@
+package ch.heigvd.amt.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.joda.time.DateTime;
+
+import javax.persistence.*;
+
+// TODO
+@Getter
+@Entity
+@Table(name = "Game")
+public class GameEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Setter
+    private DateTime timestamp;
+
+    @ManyToOne
+    @JoinColumn(name = "idTeamAway")
+    @Setter
+    private TeamEntity away;
+
+    @ManyToOne
+    @JoinColumn(name = "idTeamHome")
+    @Setter
+    private TeamEntity home;
+
+    @ManyToOne
+    @JoinColumn(name = "idReferee")
+    @Setter
+    private OfficialEntity referee;
+
+    @ManyToOne
+    @JoinColumn(name = "idUmpire")
+    @Setter
+    private OfficialEntity umpire;
+
+    @ManyToOne
+    @JoinColumn(name = "idChainJudge")
+    @Setter
+    private OfficialEntity chainJudge;
+
+    @ManyToOne
+    @JoinColumn(name = "idLineJudge")
+    @Setter
+    private OfficialEntity lineJudge;
+
+    @ManyToOne
+    @JoinColumn(name = "idBackJudge")
+    @Setter
+    private OfficialEntity backJudge;
+
+    @ManyToOne
+    @JoinColumn(name = "idSideJudge")
+    @Setter
+    private OfficialEntity sideJudge;
+
+    @ManyToOne
+    @JoinColumn(name = "idFieldJudge")
+    @Setter
+    private OfficialEntity fieldJudge;
+}
