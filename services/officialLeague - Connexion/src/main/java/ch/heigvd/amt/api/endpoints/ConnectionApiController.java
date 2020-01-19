492da9926e2229cf8620f147296a12764bb5ed0d
David Jaquet <djaquet5@gmail.com>
2020-01-19 01:29:36 +0100

    connection should be ok, need to test it...

--- services/officialLeague - Connexion/src/main/java/ch/heigvd/amt/api/endpoints/ConnectionApiController.java
+++ services/officialLeague - Connexion/src/main/java/ch/heigvd/amt/api/endpoints/ConnectionApiController.java
@@ -2,22 +2,40 @@ package ch.heigvd.amt.api.endpoints;
 
 import ch.heigvd.amt.api.ConnectionApi;
 import ch.heigvd.amt.api.model.CredentialDTO;
-import ch.heigvd.amt.api.model.Token;
+import ch.heigvd.amt.api.service.Authentication;
+import ch.heigvd.amt.api.service.JWTToken;
+import ch.heigvd.amt.entities.UserEntity;
 import ch.heigvd.amt.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import javax.validation.Valid;
+import java.util.NoSuchElementException;
 
 public class ConnectionApiController implements ConnectionApi {
 
     @Autowired
     UserRepository userRepository;
 
-    // TODO : on est dans la merde
+    @Autowired
+    Authentication authentication;
+
+    @Autowired
+    JWTToken jwtToken;
+
     @Override
-    public ResponseEntity<Token> login(@Valid CredentialDTO creds) {
-//        userRepository.findByEmail(creds.getEmail())
-        return null;
+    public ResponseEntity<String> login(@Valid CredentialDTO creds) {
+        try {
+            UserEntity userEntity = userRepository.findByEmail(creds.getEmail());
+
+            if(!authentication.checkPassword(creds.getPassword(), userEntity.getPassword()))
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+            return ResponseEntity.ok(jwtToken.createToken(userEntity));
+
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 }
